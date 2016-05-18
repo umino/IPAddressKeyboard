f53770a8ea19fd15528b78b64c5b44f1db5f1325
戸嶋誠 <toshima@abt.jp>
2016-05-18 15:45:30 +0900

    Initial Commit

---
+++ IPTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IPTextField
+//
+//  Created by 戸嶋誠 on 2016/05/18.
+//  Copyright © 2016年 戸嶋誠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
